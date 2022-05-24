0afdafeafd2843a1381b96943ccd04bf53c68049
piy-bebe <piy.bebe@gmail.com>
2022-05-24 14:40:57 +0300

    create Menu

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import './App.css';
-import WhoAreYou from "./Components/WhoAreYou/WhoAreYou";
-import Footer from "./Components/Footer/Footer";
-import {Route} from "react-router-dom";
+import './App.css'
+import WhoAreYou from './Components/WhoAreYou/WhoAreYou'
+import Footer from './Components/Footer/Footer'
+import { Route } from 'react-router-dom'
+import Main from './Components/pages/main/Main'
 
 function App() {
-    return (
-        <div className="App">
-            <WhoAreYou/>
-            <Route path={'/'} render={()=><Footer/>}/>
-            {/*<Route path='*' render={() => alert("404")}/>*/}
-        </div>
-    );
+  return (
+    <div className="App">
+      {/* <WhoAreYou /> */}
+      <Route path={'/main'} render={() => <Main />} />
+      {/* <Route path={'/'} render={() => <Footer />} /> */}
+      {/*<Route path='*' render={() => alert("404")}/>*/}
+    </div>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Components/pages/main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Menu from './blocks/Menu/Menu'
+
+// STYLES
+import style from './main.module.scss'
+
+const Main = () => {
+  return (
+    <>
+      <Menu />
+    </>
+  )
+}
+
+export default Main
