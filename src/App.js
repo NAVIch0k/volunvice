2b455c0a6c310b436bbb283f5c469e7bd2306491
piy-bebe <piy.bebe@gmail.com>
2022-05-27 09:54:04 +0300

    create Events

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import WhoAreYou from "./Components/WhoAreYou/WhoAreYou";
-import Footer from "./Components/Footer/Footer";
-import {Route, useHistory} from "react-router-dom";
-import Auth from "./Components/Auth/Auth";
-import Registration_volunteer from "./Components/Registration/Registration_volunteer/Registration_volunteer";
-import Registration_needy from "./Components/Registration/Registration_needy/Registration_needy";
+import './App.css'
+import WhoAreYou from './Components/WhoAreYou/WhoAreYou'
+import Footer from './Components/Footer/Footer'
+import { Route, useHistory } from 'react-router-dom'
+import Auth from './Components/Auth/Auth'
+import Registration_volunteer from './Components/Registration/Registration_volunteer/Registration_volunteer'
+import Registration_needy from './Components/Registration/Registration_needy/Registration_needy'
 
 function App() {
+  // let history = useHistory()
+  // history.push('/registration')
 
-    // let history = useHistory()
-    // history.push('/registration')
-
-    return (
-        <div className="App">
-            <Route exact path={'/registration'} render={() => <WhoAreYou/>}/>
-            <Route path={'/auth'} render={() => <Auth/>}/>
-            <Route path={'/registration/volunteer'} render={() => <Registration_volunteer/>}/>
-            <Route path={'/registration/needy'} render={() => <Registration_needy/>}/>
-            <Route path={'/'} render={() => <Footer/>}/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Route exact path={'/registration'} render={() => <WhoAreYou />} />
+      <Route path={'/auth'} render={() => <Auth />} />
+      <Route path={'/registration/volunteer'} render={() => <Registration_volunteer />} />
+      <Route path={'/registration/needy'} render={() => <Registration_needy />} />
+      <Route path={'/'} render={() => <Footer />} />
+    </div>
+  )
 }
- export default App
+export default App
---
+++ src/Components/pages/events/Events.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Events = () => {
+  return <section>123</section>
+}
+
+export default Events
