34104739673c0415845a587bbd0ade0a44996594
ivan <ivan.safonov.09@mail.ru>
2022-05-27 09:49:29 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import './App.css';
 import WhoAreYou from "./Components/WhoAreYou/WhoAreYou";
 import Footer from "./Components/Footer/Footer";
@@ -9,8 +8,8 @@ import Registration_needy from "./Components/Registration/Registration_needy/Reg
 
 function App() {
 
-    let history=useHistory()
-    history.push('/registration')
+    // let history = useHistory()
+    // history.push('/registration')
 
     return (
         <div className="App">
@@ -21,25 +20,5 @@ function App() {
             <Route path={'/'} render={() => <Footer/>}/>
         </div>
     );
-// =======
-// import './App.css'
-// import WhoAreYou from './Components/WhoAreYou/WhoAreYou'
-// import Footer from './Components/Footer/Footer'
-// import { Route } from 'react-router-dom'
-// import Main from './Components/pages/main/Main'
-// import Profile from './Components/pages/profile/Profile'
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* <WhoAreYou /> */}
-//       <Route path={'/main'} render={() => <Main />} />
-//       <Route path={'/profile'} render={() => <Profile />} />
-//       {/* <Route path={'/'} render={() => <Footer />} /> */}
-//       {/*<Route path='*' render={() => alert("404")}/>*/}
-//     </div>
-//   )
-// >>>>>>> origin/mainpage
-// }
-//
-// export default App
+}
+ export default App
