a3cfa8165066966c48e4c70430fd6ec7a6a473e6
ivan <ivan.safonov.09@mail.ru>
2022-06-06 23:52:28 +0300

    header

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import { NavLink } from 'react-router-dom'
 
 // STYLES
 import style from './menu.module.scss'
 
 const Menu = () => {
+
+  const { f_name, photo_name } = useSelector(state => state.user)
+
   return (
     <section className={style.menu}>
       <h2 className={style.menu__logo}>Volunvice</h2>
@@ -24,9 +28,18 @@ const Menu = () => {
         <NavLink className={[style.menu__link, style.menu__help].join(' ')} to="/">
           Хочу помочь!
         </NavLink>
-        <NavLink className={[style.menu__link, style.menu__link_underline].join(' ')} to="/auth">
-          Войти
-        </NavLink>
+        {
+          f_name ?
+            <NavLink to={'/profile'}>
+              <div className={style.menu__user}>
+                <p>{f_name}</p>
+                <img src={`http://188.225.83.101/api/static/accounts/img/${photo_name}`} alt="avatar" />
+              </div>
+            </NavLink>
+            : <NavLink className={[style.menu__link, style.menu__link_underline].join(' ')} to="/auth">
+              Войти
+            </NavLink>
+        }
       </nav>
     </section>
   )
